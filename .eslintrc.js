24490546e12e8b30733ed5b74966cff30f0df7ec
mbparis <briceparis@gmail.com>
2021-04-23 18:53:37 -0500

    Basic typescript-eslint rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,6 @@
 module.exports = {
-  parserOptions: {
-    sourceType: 'module'
-  },
-  parser: 'babel-eslint',
-  env: {
-    node: true
-  },
-  extends: [
-    'standard',
-    'prettier',
-    'prettier/standard',
-    'plugin:jest/recommended'
-  ],
-  plugins: ['prettier', 'jest'],
-  rules: {
-    'promise/catch-or-return': 'error',
-    'prettier/prettier': [
-      'error',
-      {
-        'singleQuote': true,
-        'semi': false
-      }
-    ]
-  }
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
 }
